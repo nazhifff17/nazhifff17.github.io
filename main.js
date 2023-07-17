dd9b421fdd496c61b1b87dfd7216af330c19374f
nazhifff17 <139779884+nazhifff17@users.noreply.github.com>
2023-07-17 21:26:56 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+alert ('love uuu')
+// untuk popup (alert)
+console.log ('heyooo')
+//sama saja fungsinya tapi beda halaman
+prompt('kamuu pacarnyaa siapaa?')
+// memunculkan popup dan isi
+var usia = 17
+// menampung prompt
+console.log ('heyooo usia saya adalah' + usia)
